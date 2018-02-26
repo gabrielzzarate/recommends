62f98caf76de0302073d91db148e5f9dcd6d53eb
Gabriel Zarate <gabrielzzarate@gmail.com>
2018-02-26 09:03:20 -0500

    updated production env variables

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,8 @@ module.exports = {
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY,
-	redirectDomain: process.env.REDIRECT_DOMAIN
+	redirectDomain: process.env.REDIRECT_DOMAIN,
+	fourSquareClientID: process.env.FOURSQUARE_CLIENT_ID,
+	fourSquareClientSecret: process.env.FOURSQUARE_CLIENT_SECRET
 };
 
